1b380e2132e2fb369f128f9a0532ba7361cae44c
Paul Specjał <Paul.Specjal@intrum.com>
2021-03-31 18:02:58 +0200

    working with static files

--- leads/views.py
+++ leads/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import generic
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
 
-class LandingPageView(TemplateView):
+class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
 
 def landing_page(request):
     return render(request, "landing.html")
 
-class LeadListView(ListView):
+class LeadListView(generic.ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = 'leads'
@@ -23,7 +23,7 @@ def lead_list(request):
     }
     return render(request, 'leads/lead_list.html', context)
 
-class LeadDetailView(DetailView):
+class LeadDetailView(generic.DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = 'lead'
@@ -35,7 +35,7 @@ def lead_detail(request, pk):
     }
     return render(request, 'leads/lead_detail.html', context)
 
-class LeadCreateView(CreateView):
+class LeadCreateView(generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -56,7 +56,7 @@ def lead_create(request):
     }
     return render(request, "leads/lead_create.html", context)
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -79,7 +79,7 @@ def lead_update(request, pk):
     }
     return render(request, "leads/lead_update.html", context)
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
