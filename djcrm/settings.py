c2fcfd621bd10f64b44044ecb3906b828d4fb101
Paul Specjał <Paul.Specjal@intrum.com>
2021-04-07 18:17:38 +0200

    added agents app

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'leads',
+    'agents',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_ROOT = "static_root"
 AUTH_USER_MODEL = 'leads.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads"
+LOGIN_URL = "/login"
--- leads/models.py
+++ leads/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import AbstractUser
 
 
 
 class User(AbstractUser):
     pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Lead(models.Model):
@@ -19,6 +27,15 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
+
+def post_user_create_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_create_signal, sender=User)
