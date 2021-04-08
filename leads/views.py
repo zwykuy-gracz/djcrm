0363fc1843a9c30dcc6d8afbb721dd7205310d82
Paul Specjał <Paul.Specjal@intrum.com>
2021-04-08 17:55:15 +0200

    inviting agents tro email

--- leads/views.py
+++ leads/views.py
@@ -24,8 +24,17 @@ def landing_page(request):
 
 class LeadListView(LoginRequiredMixin, generic.ListView):
     template_name = "leads/lead_list.html"
-    queryset = Lead.objects.all()
     context_object_name = 'leads'
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Lead.objects.filter(organisation=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organisation=user.agent.organisation)    
+            queryset = queryset.filter(agent__user=user)
+        return queryset
+
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -36,8 +45,16 @@ def lead_list(request):
 
 class LeadDetailView(OrganisorAndLoginRequiredMixin, generic.DetailView):
     template_name = "leads/lead_detail.html"
-    queryset = Lead.objects.all()
     context_object_name = 'lead'
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Lead.objects.filter(organisation=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organisation=user.agent.organisation)    
+            queryset = queryset.filter(agent__user=user)
+        return queryset
 
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
@@ -77,8 +94,11 @@ def lead_create(request):
 
 class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead_update.html"
-    queryset = Lead.objects.all()
     form_class = LeadModelForm
+
+    def get_queryset(self):
+        user = self.request.user
+        return Lead.objects.filter(organisation=user.userprofile)
 
     def get_success_url(self):
         return reverse("leads:lead-list")
@@ -100,10 +120,10 @@ def lead_update(request, pk):
 
 class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = "leads/lead_delete.html"
-    queryset = Lead.objects.all()
 
-    def get_success_url(self):
-        return reverse("leads:lead-list")
+    def get_queryset(self):
+        user = self.request.user
+        return Lead.objects.filter(organisation=user.userprofile)
 
 def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)
